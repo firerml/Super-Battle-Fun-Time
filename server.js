2598541be2d621fa23898b6dbffa3077b7429a9d
DBrodsky90 <DBrodsky90@gmail.com>
2014-12-15 11:38:18 -0500

    Added a port for heroku to listen on

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.set('port', (process.env.PORT || 8000))
+
 io.on('connection', function(client) {
 	console.log("Client has connected...");
 });
@@ -12,5 +14,7 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/public/views/index.html');
 });
 
-server.listen(8000);
+server.listen(app.get('port'), function() {
+	console.log("Node app is running at localhost:" + app.get('port'));
+})
 
