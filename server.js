867c7de436cb640152b9c93a2920682220640e47
DBrodsky90 <DBrodsky90@gmail.com>
2014-12-17 23:56:06 -0500

    Made connected users list update on connect and disconnect

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var jquery = require('jquery');
 app.set('port', (process.env.PORT || 8000));
 
 // Usernames which are currently connected
-var usernames = {};
+var usernames = [];
 var numUsers = 0;
 
 
@@ -28,23 +28,35 @@ io.on('connection', function(client) {
 	client.on('add user', function(username) {
 		console.log(client.id);
 		// add the clients username to the global list of users
-		usernames[username] = client.id;
+		var userObj = {};
+		userObj['name'] = username;
+		userObj['id'] = client.id;
+		usernames.push(userObj)
 		++numUsers;
+		addedUser = true;
 		// echo globally that a user has connected
-		client.broadcast.emit('user joined', {
-			username: username,
-			userID: client.id
-		});
+		client.broadcast.emit('user joined', usernames);
 		// echo object back to client
-		client.emit('user joined', {
-			username: username,
-			userID: client.id
-		});
+		client.emit('user joined', usernames);
 	});
 
 	// Listen for client emiting 'updateBullets' event
 	client.on('updateBullets', function(data) {
 		client.broadcast.emit('updateBullets',data);
+	});
+
+	// When client's socket disconnects, remove user from 
+	// array of usernames, and broadcast updated usernames
+	client.on('disconnect', function() {
+		if(addedUser) {
+			usernames.forEach(function(object) {
+				if(object['id'] == client.id) {
+					usernames.pop(object);
+					--numUsers;
+				}
+			})
+		}
+		client.broadcast.emit('user joined', usernames);
 	});
 });
 
