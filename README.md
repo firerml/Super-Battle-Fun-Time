2ee2bd6a44a1194c7c8543c3a1e7f5f274ab6e04
DBrodsky90 <DBrodsky90@gmail.com>
2014-12-22 14:26:01 -0500

    Added comments, updated readme

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ BattleTanks
 ===========
 
 A BattleTanks game using Canvas and Socket.io.
+
+Created by David Brodsky and Mike Firer for General Assembly's Web Development Immersive
+
+Can you destroy the enemy tank before the enemy tank destroys you?
+Challenge someone online right now and find out!
+
+WASD/Arrow Keys - Move forward and backward, turn left and right (tank-style controls)
+Mouse - Move turret
+Click - Shoot
+
--- public/js/game.js
+++ public/js/game.js
@@ -1,12 +1,15 @@
+// Updates the enemy tank's properties from the information received from the enemy
 function receiveUpdate(enemyTank,tankProperties) {
 	enemyTank.setAttributes(tankProperties);
 }
 
+// Checks if a point is inside the tank
 function detectCollisions(tank,point) {
   var corners = tank.getCorners();
   return isInsideSquare(corners.tL, corners.tR, corners.bR, corners.bL, point);
 }
 
+// Checks if 'p' is inside a square with corners 'a,b,c,d'
 function isInsideSquare(a,b,c,d,p) {
 	if (triangleArea(a,b,p) > 0 || triangleArea(b,c,p) > 0 || triangleArea(c,d,p) > 0 || triangleArea(d,a,p) > 0) {
 		return false;
@@ -16,10 +19,12 @@ function isInsideSquare(a,b,c,d,p) {
   }
 }
 
+// Calculates a triangles area, used for isInsideSquare
 function triangleArea(a,b,c) {
 	return (c.x*b.y - b.x*c.y) - (c.x*a.y - a.x*c.y) + (b.x*a.y - a.x*b.y);
 }
 
+// Generates a Win/Lose message on your screen
 function endMessage() {
 	var message;
 	if (myTank.gameOver > 0) message = 'You win!'; 
